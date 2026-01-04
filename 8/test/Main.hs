266f13241aa98b2099a95396a2694e554973a845
Miguel Negrao <miguel.negrao@friendlyvirus.org>
2026-01-04 17:00:00 +0000

    +

--- 8/test/Main.hs
+++ 8/test/Main.hs
@@ -1,6 +1,6 @@
 module Main (main) where
 
-import AoC (parser, part1, part2, Homework(..))
+import AoC
 import Data.Either (isRight, fromRight)
 import qualified Data.Text.IO as TIO
 import Test.Tasty (defaultMain, testGroup)
@@ -21,19 +21,14 @@ main = defaultMain $ testGroup "AoC5"
     , testCase "parser parses input" $ do
         input <- TIO.readFile "input"
         assertBool "expected parse to succeed" (isRight (parse parser "input" input))
-    , testCase "num cols equals" $ do
-        input <- TIO.readFile "input"
-        let
-            Right (Homework xs ys) = parse parser "input" input
-        assertEqual "num ops == num elements on every" (length (nub $ length ys:fmap length xs)) 1
     , testCase "part1 example" $ do
         input <- TIO.readFile "test_input"
         case parse parser "test_input" input of
             Left err -> assertFailure (show err)
-            Right parsed -> assertEqual "part1" 4 (part1 parsed)
-    , testCase "part2 example" $ do
-        input <- TIO.readFile "test_input"
-        case parse parser "test_input" input of
-            Left err -> assertFailure (show err)
-            Right parsed -> assertEqual "part2" 3263827 (part2 input parsed)
+            Right parsed -> assertEqual "part1" 40 (part1Test parsed)
+    -- , testCase "part2 example" $ do
+    --     input <- TIO.readFile "test_input"
+    --     case parse parser "test_input" input of
+    --         Left err -> assertFailure (show err)
+    --         Right parsed -> assertEqual "part2" 3263827 (part2 input parsed)
     ]
