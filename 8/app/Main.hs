4c00bdbc43b0ee10d0ff0aa9c51d90682d0827ad
Miguel Negrao <miguel.negrao@friendlyvirus.org>
2026-02-22 17:00:00 +0000

    +

--- 8/app/Main.hs
+++ 8/app/Main.hs
@@ -11,8 +11,6 @@ main = do
     input <- TIO.readFile "input"
     case parse parser "input" input of
         Right parsed -> do
-            print $ take 3 $ sortOn Down $ part1UniqueLengths 1000 parsed
-            print $ part1UniqueLengths 1000 parsed
             putStrLn $ "part1: " <> show (part1 parsed) <> "\n"
             putStrLn $ "part2: " <> show (part2 parsed) <> "\n"
         Left e -> putStrLn (errorBundlePretty e)
