80815429791e08e820c51891717927d27e20d8d7
Miguel Negrao <miguel.negrao@friendlyvirus.org>
2026-06-07 17:00:00 +0100

    +

--- 10/src/AoC.hs
+++ 10/src/AoC.hs
@@ -16,8 +16,26 @@
 part1
 time: 
 attempts: 1
-used chatgpt: yes: get documentation of Seq quickly;
+used chatgpt: yes: get documentation of Seq quickly; to find equivalent of concat for Seq.
 notes: knowing sepEndBy and between, parsing becomes easy. Initial idea is to form a Tree of choices of button pressing and then travel the tree in breadth-first keeping track of the node traveled to get to each node, and stop once the sequence gives the necessary result. 5.631 s ± 746 ms is a bit too slow.
+V2 keeps intermediate results in tree
+V3 uses bit field, now that is much better
+
+v1
+  part1 without parsing: OK
+    5.975 s ±  12 ms
+  part1 with parsing:    OK
+    6.183 s ± 378 ms
+v2 keep state
+  part1 without parsing: OK
+    4.311 s ±  12 ms
+  part1 with parsing:    OK
+    4.311 s ±  21 ms
+v3 bit field
+  part1 without parsing: OK
+    3.250 s ± 102 ms
+  part1 with parsing:    OK
+    3.133 s ± 123 ms
 
 part2
 time:
@@ -67,6 +85,7 @@ import Text.Megaparsec.Debug
 --import Linear.Metric
 import Math.Combinat.Sets (combine, choose)
 import Data.Ord (Down(..))
+import Data.Bits
 
 -- by ChatGPT
 newtype OnePerLine a = OnePerLine [a]
@@ -108,7 +127,7 @@ parser = some $ do
   newline
   return $ Machine {..}
 
-data Tree a = Leaf a | Tree a (Seq (Tree a)) deriving (Show, Eq)
+data Tree a = Tree a (Seq (Tree a)) deriving (Show, Eq)
 
 -- |
 -- To see the execution of the function in the Haskell debugger I set breakpoints in the Leaf a and Tree a cases.
@@ -116,29 +135,18 @@ bfsStopAt :: (a -> Bool) -> Tree a -> Maybe a
 bfsStopAt pred tree = go pred (Seq.singleton tree) where
   go :: (a -> Bool) -> Seq (Tree a) -> Maybe a
   go pred (Seq.viewl -> Seq.EmptyL) = Nothing
-  go pred (Seq.viewl -> (Leaf a) Seq.:< xs)
-    | pred a = Just a
-    | otherwise = go pred xs
+  -- go pred (Seq.viewl -> (Leaf a) Seq.:< xs)
+  --   | pred a = Just a
+  --   | otherwise = go pred xs
   go pred (Seq.viewl -> (Tree a ys) Seq.:< xs)
     | pred a    = Just a
-    | otherwise = go pred (xs Seq.>< ys) 
+    | otherwise = go pred (xs Seq.>< ys)
 
-treeT1 = Tree 1 (Seq.fromList [Tree 2 $ Seq.fromList [Leaf 3, Leaf 4], Tree 5 $ Seq.fromList [Leaf 6, Leaf 7, Leaf 8, Leaf 99]])
-
-treeT2 = bfsStopAt (> 8) treeT1
-
--- | todo: I don't think I even have to keep the whole sequence I just need to keep the intermediate result
 bfsStopAtPath :: forall a. Show a => Int -> ((Seq a) -> Bool) -> Tree a -> Maybe (Seq a)
 bfsStopAtPath max pred tree = go 0 pred $ Seq.singleton (Seq.Empty, tree) where
   go :: Int -> (Seq a -> Bool) -> Seq (Seq a, Tree a) -> Maybe (Seq a)
   -- No more nodes to process, stop
   go _ pred (Seq.viewl -> Seq.EmptyL) = Nothing
-  go n pred (Seq.viewl -> (zs, Leaf a) Seq.:< xs)
-    | n > max = Nothing
-    -- found element, stop
-    | pred (zs Seq.|> a) = Just $ zs Seq.|> a
-    -- go on to next elements in list to process
-    | otherwise = go (n + 1) pred xs
   go n pred (Seq.viewl -> (zs, Tree a ys) Seq.:< xs)
     | n > max = Nothing
     -- found element, stop
@@ -146,10 +154,22 @@ bfsStopAtPath max pred tree = go 0 pred $ Seq.singleton (Seq.Empty, tree) where
     -- add all branches of this tree to the list of nodes to process
     | otherwise = go (n + 1) pred $ xs Seq.>< (fmap (\b -> (zs Seq.|> a, b)) ys)
 
-t3 = bfsStopAtPath 4 (\xs -> length xs == 3) treeT1
 
+part1 = part1v1
 
--- >>> t3
+part1v1 :: ParsedType -> Int
+part1v1 xs = (sum smallestSets) where -- trace ("smallestSets = " <> show smallestSets) 
+  smallestSets = fmap processMachineV1 xs
+
+processMachineV1 (Machine pattern buttons _) = case res of
+      Just ys -> Seq.length ys - 1 -- trace ("ys = " <> show ys) $ 
+      Nothing -> error "part1 cannot find solution"
+    where 
+      res = bfsStopAtPath 100000000 pred $ part1BuildList Seq.empty buttons'
+      buttons' = Seq.fromList $ fmap Seq.fromList buttons
+      startPattern = Seq.replicate (length pattern) False
+      seqPattern = Seq.fromList pattern
+      pred xs = applyButtons startPattern xs == seqPattern
 
 -- I love lazy data structures !!
 -- | this builds a tree like bi are buttons
@@ -157,28 +177,64 @@ t3 = bfsStopAtPath 4 (\xs -> length xs == 3) treeT1
 --      b1              b2            ...    bn      
 --b1 b2 ..bn       b1 b2... bn            b1 b2 ... bn  
 --                      ...
-part1BuildList :: [[a]] -> Tree [a]
-part1BuildList xs = Tree [] $ go $ Seq.fromList xs where
+part1BuildList :: a -> Seq a -> Tree a
+part1BuildList init xs = Tree init $ go xs where
   go xs = fmap f xs where
     f x = Tree x $ go xs
 
-applyButtons :: Seq Bool -> Seq [Int] -> Seq Bool
-applyButtons xs ys = foldr f xs (concat $ toList ys) where
+applyButtons :: Seq Bool -> Seq (Seq Int) -> Seq Bool
+applyButtons xs ys = foldr f xs (fold ys) where
   f = Seq.adjust' not
 
-processMachineV1 (Machine pattern buttons _) = case a of
-      Just ys -> Seq.length ys - 1 -- trace ("ys = " <> show ys) $ 
+-- V2 Keep just intermediate results while traversing the tree
+
+part1v2 :: ParsedType -> Int
+part1v2 xs = (sum smallestSets) where -- trace ("smallestSets = " <> show smallestSets) 
+  smallestSets = fmap processMachineV2 xs
+
+applyButton :: Seq Bool -> Seq Int -> Seq Bool
+applyButton pattern button = foldr (Seq.adjust' not) pattern button
+
+processMachineV2 (Machine pattern buttons _) =  case res of
+      Just m -> m -- trace ("ys = " <> show ys) $ 
       Nothing -> error "part1 cannot find solution"
-    where 
-      a = bfsStopAtPath 100000000 pred $ part1BuildList buttons
-      startPattern = Seq.replicate (length pattern) False
+    where
+      startPattern = Seq.replicate (length pattern) False 
+      buttons' = Seq.fromList $ fmap Seq.fromList buttons
+      res = bfsStopAtPath2 startPattern 100000000 f $ part1BuildList Seq.empty buttons'
       seqPattern = Seq.fromList pattern
-      pred xs = applyButtons startPattern xs == seqPattern
+      f pattern button = let b' = foldr (Seq.adjust' not) pattern button in (b', b' == seqPattern)
 
+bfsFoldStop :: forall a b. Show a => b -> Int -> (b -> a -> (b, Bool)) -> Tree a -> Maybe Int
+bfsFoldStop init max f tree = go 0 $ Seq.singleton (init, 0, tree) where
+  go :: Int -> Seq (b, Int, Tree a) -> Maybe Int
+  -- No more nodes to process, stop
+  go n (Seq.viewl -> Seq.EmptyL) = Nothing
+  go n (Seq.viewl -> (b, m, Tree a ys) Seq.:< xs)
+    | n > max = Nothing
+    -- found element, stop
+    | stop = Just m
+    -- add all branches of this tree to the list of nodes to process
+    | otherwise = go (n + 1) $ xs Seq.>< (fmap (\y -> (b', m+1, y)) ys)
+    where
+      (b', stop) = f b a
 
-part1 :: ParsedType -> Int
-part1 xs = (sum smallestSets) where -- trace ("smallestSets = " <> show smallestSets) 
-  smallestSets = fmap processMachineV1 xs
+part1v3 :: ParsedType -> Int
+part1v3 xs = (sum smallestSets) where -- trace ("smallestSets = " <> show smallestSets) 
+  smallestSets = fmap processMachineV3 xs
+
+processMachineV3 (Machine pattern buttons _) =  case res of
+      Just m -> m -- trace ("ys = " <> show ys) $ 
+      Nothing -> error "part1 cannot find solution"
+    where
+      startPattern = 0
+      buttons' :: Seq Integer
+      buttons' = Seq.fromList $ fmap toInteger buttons
+      toInteger :: [Int] -> Integer
+      toInteger = foldr (flip setBit) 0
+      res = bfsFoldStop startPattern 100000000 f $ part1BuildList 0 buttons'
+      intPattern =  foldr (\(i, isOn) x -> if isOn then setBit x i else clearBit x i) 0 $ zip [0..] pattern
+      f pattern button = let b' = pattern `xor` button in (b', b' == intPattern)
 
 -- | right now test doesn't even find the right answer and need to check if I'm keeping the intermediate calculations
 part2 :: ParsedType -> Int
