7e652fd804395c9d249ec080a65531b9103bc95a
Miguel Negrao <miguel.negrao@friendlyvirus.org>
2026-06-07 17:00:00 +0100

    12 part 1 done !

--- 12/src/AoC.hs
+++ 12/src/AoC.hs
@@ -17,8 +17,17 @@
 part1
 time: 
 attempts:
-used chatgpt: yes, to get some help on pinpointing the memory leak. Asked for very generic analysis of my approach, essentially if it could in theory work or not. It wasn't very clear what the answer was, I think it was, perhaphs with some changes. She suggests moving to bit as Integer. My input max seems to be 50x50 that is 2500 bits
-notes: First attempt blows up in memory even in the test case. Need to make stuff less lazy ?
+used chatgpt: yes.
+- to get some help on pinpointing the memory leak. Asked for very generic analysis of my approach, essentially if it could in theory work or not. It wasn't very clear what the answer was, I think it was, perhaphs with some changes. 
+- She suggests moving to bit as Integer. My input max seems to be 50x50 that is 2500 bits, will use Integer. 
+- She detected an error with my rotation, fixed that. 
+- Suggested speedup by checking impossibility in number of live cells vs total cells of the grid.
+notes: My initial idea was, generate all ways to put the pattens on the area and then check each one. I choose Vector Bool in 1D, I thought that would be fast enough. First attempt blows up on the test case in memory (and time). Felling stuck, chatgpt hinted at somehow fusing the generations and consumption of the array and also switching to bitfields. Once I switched to bitfield I saw this simplified a lot because a present placed in given point and rotation is just one Intenger, and checking and placing is just binary AND and OR of Integers. Still that did not fix the memory leak. For v3 I redid the list monad this time using guards to not generate any further when then first elements fail. v3 has the memory under control, but takes 213s on the test, not good, that doesn't work on the main input, will not finish ever. Suggested by ChatGPT first checked if the number of live cells even fits in the area and aborts straight away if not, with that it runs in 29s.
+
+part1 without parsing: OK
+    28.067 s ± 2.23 s
+part1 with parsing:    OK
+    29.365 s ± 5.4 ms
 
 part2
 time:
@@ -132,6 +141,7 @@ flipX xs = fmap f xs where
 
 -- |
 -- Given a list of lists generate all ways of picking one element out of every list, returning them in a list in the same order as the lists that were given.
+-- I think with this version I'm generating many combinations that I would already know they don't work because the first n elements already don't work.
 choices :: [[x]] -> [[x]]
 choices xs0 = {-# SCC "choices" #-} go xs0 where
   go [] = [[]]
@@ -147,13 +157,53 @@ choices xs0 = {-# SCC "choices" #-} go xs0 where
 -- >>> choices [[1,2],[5,6,7],[8,9]]
 -- [[1,5,8],[1,5,9],[1,6,8],[1,6,9],[1,7,8],[1,7,9],[2,5,8],[2,5,9],[2,6,8],[2,6,9],[2,7,8],[2,7,9]]
 
+part1 :: ParsedType -> Int
+part1 = part1v3
+
 -- |
 -- For each region we try to make it work. We then count the number of regions that work.
 -- For each region we generate all ways we can put the shapes in the region and check if any works.
-part1 :: ParsedType -> Int
-part1 (shapes, regions) = length $ trace ("part1: " <> show regionChecks <> "\n") filter id regionChecks where
+part1V1 :: ParsedType -> Int
+part1V1 (shapes, regions) = length $ trace ("part1: " <> show regionChecks <> "\n") filter id regionChecks where
   regionChecks = fmap f regions
   f region@(dims, indexes) =  any checkListRegions $  createPossibilities shapes region
+
+-- |
+-- I believe this will build all areas that work starting from the last shapes to place. and then try adding one more shape. Probably still to much memory, it's not exactly this.
+part1v2 :: ParsedType -> Int
+part1v2 (shapes, regions) = length $ trace ("part1: " <> show regionChecks <> "\n") filter id regionChecks where
+  regionChecks = fmap f regions
+  f region@(dims, indexes) = not $ null $ go allPossibilites where  
+    allPossibilites = createPossibilities' shapes region
+    go :: [[Integer]] -> [Integer] -- keep just final patterns that work
+    go [] = [0]
+    go (y:ys) =
+      do
+        z <- y
+        area <- go ys
+        guard $ area .&. z == 0 
+        return $ z .|. area
+
+-- | memory usage tiny 0.1%
+-- It works, but took 213s... that's not going to work on input...
+part1v3 :: ParsedType -> Int
+part1v3 (shapes, regions) = length $ filter id regionChecks where
+  -- length $ trace ("part1: " <> show regionChecks <> "\n") filter id regionChecks where
+  regionChecks = fmap f regions
+  f region@(dims, indexes) = checkFits shapes region && (not $ null $ go 0 allPossibilites) where  
+    allPossibilites = createPossibilities' shapes region
+    go :: Integer -> [[Integer]] -> [[Integer]] -- keep just final patterns that work
+    go area0 [] = [[]]
+    go area0 (y:ys) =
+      do
+        z <- y
+        guard $ area0 .&. z == 0
+        zs <- go (area0 .|. z) ys
+        return $ z:zs
+
+checkFits :: [Shape] -> (Dimensions, [Index]) -> Bool
+checkFits shapes ((w,h), is) = (w*h) >= n where
+    n = sum $ zipWith (*) (fmap length shapes) is
 
 generateBitField :: Width -> Point -> Shape -> Integer
 generateBitField width topLeft shape = foldr g 0 indexes where
@@ -179,6 +229,21 @@ createPossibilities shapes (dims@(width, height), indexes) = {-# SCC "createPoss
       y <- [0..maxH]
       return (x,y)
 
+createPossibilities' :: [[Point]] -> ((Int, Int), [Int]) -> [[Integer]]
+createPossibilities' shapes (dims@(width, height), indexes) = shapes3 where
+    (maxW, maxH) = addPoint dims (-3, -3)
+    shapes3 = {-# SCC "createPossibilities.shapes3" #-} concat $ zipWith f shapes2 indexes
+    f xs n = {-# SCC "createPossibilities.replicate" #-} replicate n xs
+    shapes2 = {-# SCC "createPossibilities.shapes2" #-} fmap g shapes
+    genBitField2 = generateBitField width
+    g shape = {-# SCC "createPossibilities.shapeOptions" #-} do
+      p <- positions
+      s <- generateRotationsFlips shape
+      return $ genBitField2 p s
+    positions = {-# SCC "createPossibilities.positions" #-} do
+      x <- [0..maxW]
+      y <- [0..maxH]
+      return (x,y)
 
 -- |
 -- generate all rotations and flips of this shape
@@ -239,6 +304,38 @@ parser = do
 
 part2 = undefined
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 -- Tests
 
 genTestString :: IO ()
@@ -273,61 +370,3 @@ tFold5 = tFold4 0 [1..3]
 -- nice, short circuit accomplished ! I just have to fuse the f with foldr and shortcircuit when neeed.
 tFold6 = tFold4 0 [1..]
 
-
-
--- >>> OnePerLine tParsed
--- not parsed
-
--- >>> OnePerLine $ choose 2 tParsed
--- [(7,1),(11,1)]
--- [(7,1),(11,7)]
--- [(7,1),(9,7)]
--- [(7,1),(9,5)]
--- [(7,1),(2,5)]
--- [(7,1),(2,3)]
--- [(7,1),(7,3)]
--- [(11,1),(11,7)]
--- [(11,1),(9,7)]
--- [(11,1),(9,5)]
--- [(11,1),(2,5)]
--- [(11,1),(2,3)]
--- [(11,1),(7,3)]
--- [(11,7),(9,7)]
--- [(11,7),(9,5)]
--- [(11,7),(2,5)]
--- [(11,7),(2,3)]
--- [(11,7),(7,3)]
--- [(9,7),(9,5)]
--- [(9,7),(2,5)]
--- [(9,7),(2,3)]
--- [(9,7),(7,3)]
--- [(9,5),(2,5)]
--- [(9,5),(2,3)]
--- [(9,5),(7,3)]
--- [(2,5),(2,3)]
--- [(2,5),(7,3)]
--- [(2,3),(7,3)]
-
-
-
--- Is this really slow ?
--- putShapeInRegion :: Int -> Int -> Shape -> Point -> Shape -> Maybe Shape
--- putShapeInRegion width height region (x,y) shape
---   | (x + 3 > width) || (y + 3 > height) = Nothing
---   | not $ Set.disjoint region shapeTranslated = Nothing
---   | otherwise = Just $ Set.union region shapeTranslated
---   where
---     shapeTranslated = fmap (addPoint p) shape
-
--- could be optimized by geting row only once
--- anyTrueSubregion v (x,y) = any id values where 
---   positions = do
---     i <- [0..2]
---     j <- [0..2]
---     return (x + i , y + j)
---   values = fmap (\pos -> shapeAt pos v) positions
-
-
-
---shapeAt :: Point -> Shape -> Bool
---shapeAt (x,y) v = (v V.! y) V.! x
