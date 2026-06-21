de6b4429445b5c764a2dc2a552139031cb4e2400
Miguel Negrao <miguel.negrao@friendlyvirus.org>
2026-06-21 17:00:00 +0100

    clean 12

--- 9/src/AoC.hs
+++ 9/src/AoC.hs
@@ -7,6 +7,8 @@
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
 {- HLINT ignore "Unused LANGUAGE pragma" -}
 
 {--
@@ -22,7 +24,7 @@ attempts: 1
 used chatgpt: yes, to research computacional geometry alrightms in general, but also wikipedia. It was used to determine the algorithmn for the function polygonInsidePolygonTouchingOk. ChatGPT as used to help create sbv proofs of correctness for my hand-coded functions.  All the code in thihs file is my own.
 notes:
 I decided to first research a bit the algorithms used for this, as this seemed a bit too far away from the usual CS algorithms.
-The algorithm was a too tricky to find on my own because touching edges and vertices must be allowed. In the end I needed help for the algorithmn of one of the functions. The first try was correct, clocking at 4m execution time. Will try to make it more eficient.
+The algorithm was a too tricky to find on my own because touching edges and vertices must be allowed. In the end I needed help for the algorithmn of one of the functions. The first try was correct, clocking at 4m execution time (used Rational). Will try to make it more eficient (and still correct).
 
 times:
 1st attempt: 4m
@@ -97,16 +99,20 @@ instance Show a => Show (OnePerLine a) where
 
 type Parser = Parsec Void Text
 
-type Point = (Int,Int)
-type Vertice = Point
-type PolygonVertices = [Vertice]
-type Edge = ((Double, Double), (Double, Double))
+-- This uses just for smart type aliases
+type family Point a
+type instance Point a = (a,a)
+
+type family Edge a
+type instance Edge a = (Point a, Point a)
+
+type PolygonVertices = [Point Int]
 type ParsedType = PolygonVertices
 
 pNumber :: forall a. Read a => Parser a
 pNumber = read <$> some digitChar
 
-pNumberLine :: Parser Point
+pNumberLine :: Parser (Point Int)
 pNumberLine = do
     x <- pNumber
     char ','
@@ -114,25 +120,25 @@ pNumberLine = do
     eol
     return (x,y)
 
-parser :: Parser ParsedType
+parser :: Parser [Point Int]
 parser = some pNumberLine
 
-area :: Num a => (a, a) -> (a, a) -> a
+area :: Num a => Point a -> Point a -> a
 area (x1,y1) (x2,y2) = (abs dx + 1) * (abs dy + 1) where
   dx = x1 - x2 
   dy = y1 - y2
 
-part1 :: ParsedType -> Int
+part1 :: [Point Int] -> Int
 part1 xs =  case ys of
         (y:_) -> y
         _ -> 0
     where
         ys = sortOn Down $ fmap (uncurry area) ((\[a,b] -> (a,b)) <$> choose 2 xs)
 
-pointSum :: Num a => (a,a) -> (a,a) -> (a,a)
+pointSum :: Num a => Point a -> Point a -> Point a
 pointSum (x1,y1) (x2,y2) = (x1+x2,y1+y2)
 
-det2x2 :: Num a => ((a,a),(a,a)) -> a
+det2x2 :: Num a => (Point a, Point a) -> a
 det2x2 ((a1,b1), (a2,b2)) = a1*b2 - a2*b1
 
 -- |
@@ -140,13 +146,13 @@ det2x2 ((a1,b1), (a2,b2)) = a1*b2 - a2*b1
 -- solve
 -- a1 b1 x x = c1
 -- a2 b2   y   c2 
-solve2x2' :: Fractional a => ((a,a),(a,a)) -> (a,a) -> a -> (a,a)
+solve2x2' :: Fractional a => (Point a, Point a) -> Point a -> a -> Point a
 solve2x2' a@((a1,b1),(a2,b2)) (c1,c2) det = ( (c1*b2-b1*c2) / det, (a1*c2 - c1 * a2) / det )
 
 -- |
 -- Cramers rule
 -- Returns a unique solution if it exists. If there are many solutions or no solution returns Nothing.
-solve2x2 :: (Fractional a, Eq a) => ((a,a),(a,a)) -> (a,a) -> Maybe (a,a)
+solve2x2 :: (Fractional a, Eq a) => (Point a, Point a) -> Point a -> Maybe (Point a)
 solve2x2 a@((a1,b1),(a2,b2)) c@(c1,c2) = if det /= 0 then Just res else Nothing where
     det = det2x2 a
     res = solve2x2' a c det
@@ -169,21 +175,21 @@ can be checked by determinant of vectors as lines of matrix is zero
 
 (y2 - y1)*w + (x1 - x2)*z = x1*y2 - x2*y1
 --}
-lineIntersectionMatrix :: Num a => ((a, a), (a, a)) -> ((a, a), (a, a)) -> ((a, a), (a, a))
+lineIntersectionMatrix :: Num a => Edge a -> Edge a -> (Point a, Point a)
 lineIntersectionMatrix ((x1, y1), (x2, y2)) ((x3, y3), (x4, y4)) =
     ((y2 - y1, x1-x2), (y4 - y3, x3 - x4))
 
-lineIntersectionConstants :: Num a => ((a, a), (a, a)) -> ((a, a), (a, a)) -> (a, a)
+lineIntersectionConstants :: Num a => Edge a -> Edge a -> Point a
 lineIntersectionConstants ((x1, y1), (x2, y2)) ((x3, y3), (x4, y4)) =
     (x1 * y2 - x2 * y1, x3 * y4 - x4 * y3)
 
 isInInterval :: Ord a => a -> a -> a -> Bool
 isInInterval x1 x2 x3 = if x1 <= x2 then (x1 <= x3 && x3 <= x2) else (x2 <= x3 && x3 <= x1)
 
-isInRect :: (Ord a1, Ord a2) => ((a1, a2), (a1, a2)) -> (a1, a2) -> Bool
+isInRect :: (Ord a) => Edge a -> Point a -> Bool
 isInRect ((x1,y1), (x2,y2)) (x3,y3) = isInInterval x1 x2 x3 && isInInterval y1 y2 y3
 
-lineSegmentsIntersectAtInteriorPoint :: (Ord a2, Fractional a2, Show a2) => ((a2, a2), (a2, a2)) -> ((a2, a2), (a2, a2)) -> Bool
+lineSegmentsIntersectAtInteriorPoint :: (Ord a, Fractional a, Show a) => Edge a -> Edge a -> Bool
 lineSegmentsIntersectAtInteriorPoint a@(p1, p2) b@(p3, p4)
     | p1 == p2 = error $ "lineSegmentsIntersectAtInteriorPoint: first segment has identical endpoints: " <> show p1 <> " " <> show p2
     | p3 == p4 = error "lineSegmentsIntersectAtInteriorPoint: second segment has identical endpoints"
@@ -198,7 +204,7 @@ lineSegmentsIntersectAtInteriorPoint a@(p1, p2) b@(p3, p4)
     where
         sol = solve2x2 (lineIntersectionMatrix a b) (lineIntersectionConstants a b)
 
-lineSegmentIntersectsHalfRayGoingRight :: (Ord a, Fractional a) => (a,a) -> ((a, a), (a, a)) -> Bool
+lineSegmentIntersectsHalfRayGoingRight :: (Ord a, Fractional a) => Point a -> Edge a -> Bool
 lineSegmentIntersectsHalfRayGoingRight pointInAnalysis@(x3,_) a@(p1, p2)
     | p1 == p2 = error "lineSegmentsIntersectAtInteriorPoint: first segment has identical endpoints"
     | otherwise = case sol of
@@ -213,7 +219,7 @@ lineSegmentIntersectsHalfRayGoingRight pointInAnalysis@(x3,_) a@(p1, p2)
         b = (pointInAnalysis, pointSum pointInAnalysis (1,0))
         sol = solve2x2 (lineIntersectionMatrix a b) (lineIntersectionConstants a b)
 
-memoIntegralPoint :: Integral a => Memo (a,a)
+memoIntegralPoint :: Integral a => Memo (Point a)
 memoIntegralPoint = Memo.pair Memo.integral Memo.integral
 
 memoRational :: Memo Rational
@@ -221,15 +227,15 @@ memoRational = Memo.wrap to from memoIntegralPoint where
     to (a,b) = a % b
     from r = (numerator r , denominator r)
 
-memoRationalPointPoint :: Memo (Rational,Rational)
+memoRationalPointPoint :: Memo (Point Rational)
 memoRationalPointPoint = Memo.pair memoRational memoRational
 
-memoEdge :: Memo ((Rational,Rational), (Rational,Rational))
+memoEdge :: Memo (Edge Rational)
 memoEdge = Memo.pair memoRationalPointPoint memoRationalPointPoint
 
 memoLineSegmentIntersectsHalfRayGoingRight = Memo.memo2 memoRationalPointPoint memoEdge lineSegmentIntersectsHalfRayGoingRight
 
-verticesToEdges' :: [a] -> [(a, a)]
+verticesToEdges' :: [Point a] -> [Edge a]
 verticesToEdges' xs = zipWith f xs (drop 1 $ cycle xs) where
     n = length xs
     f a b = (a,b)
@@ -245,6 +251,7 @@ intToDouble = fromIntegral
 
 -- |
 -- Rational should not have precision problems, but maybe too slow.
+polygonEdgesProperlyIntersect :: (Ord a, Fractional a, Show a) => [Edge a] -> [Edge a] -> Bool
 polygonEdgesProperlyIntersect edges_a edges_b = not $ null intesectingEdges where
     intesectingEdges = do
         edge_a <- edges_a
@@ -285,16 +292,16 @@ polygonEdgesProperlyIntersect edges_a edges_b = not $ null intesectingEdges wher
 --                  |                     |
 --                  +---------------------+
 --                      1.036 s ±  70 ms
+
+pointInPolygon :: (Ord a, Fractional a, Integral b) => [Edge a] -> Point b -> Bool
 pointInPolygon edges p = odd numberOfCrossings where
-    p' = over both intToDouble p
+    p' = over both fromIntegral p
     numberOfCrossings = length $ filter f edges
     f edge = lineSegmentIntersectsHalfRayGoingRight p' edge
 
+memoPointInPolygon  :: (Ord a, Fractional a, Integral b) => [Edge a] -> Point b -> Bool
 memoPointInPolygon edges p = memoIntegralPoint memoPointInPolygon' p where
-    memoPointInPolygon' p = odd numberOfCrossings where
-        p' = over both intToDouble p
-        numberOfCrossings = length $ filter f edges
-        f edge = lineSegmentIntersectsHalfRayGoingRight p' edge
+    memoPointInPolygon' = pointInPolygon edges
 
 -- |
 -- After tring a couple of times on my own, I didn't manage to find a criteria that worked, so I asked ChatGPT for help:
@@ -302,19 +309,25 @@ memoPointInPolygon edges p = memoIntegralPoint memoPointInPolygon' p where
 -- 2. No edge of A may strictly/properly cross an edge of B.
 -- 3. Ignore boundary touches and collinear overlaps.
 -- Todo: check in SBV
+polygonInsidePolygonTouchingOk :: (Show a, Fractional a, Ord a, Integral b) => ([Point b], [Edge a]) -> ([Point b], [Edge a]) -> Bool
 polygonInsidePolygonTouchingOk a@(verticesA@(a1:a2:a3:_),edges_a) b@(verticesB@(b1:b2:b3:_),edges_b) = everyVerticeOfAinsideB && not (polygonEdgesProperlyIntersect edges_a edges_b) where
     everyVerticeOfAinsideB = all (memoPointInPolygon edges_b) verticesA
 polygonInsidePolygonTouchingOk _ _ = error "polygonInsidePolygonTouchingOk: Both polygons must have 3 vertices"
 
---  a +-------------+ c
+--  a +-------------+ d
 --    |             |
---  d +-------------+ b
+--  c +-------------+ b
+makeRectangle :: Point Int -> Point Int -> ([Point Int], [Edge Double])
 makeRectangle a@(x1,y1) b@(x2,y2) = (vertices, edges) where
     c = (x1,y2)   
     d = (x2,y1)
     vertices = [a,b,c,d]
     edges = verticesToEdges vertices
-    
+
+-- |
+-- Rational would give guarantees of no rounding error, but is much more expensive.
+-- Double gives the right solution in my input    
+verticesToEdges :: [Point Int] -> [Edge Double]
 verticesToEdges = verticesToEdges' . over (traversed.each) intToDouble
 
 part2 :: PolygonVertices -> Int
@@ -322,6 +335,7 @@ part2 vertices = case areas of
         (x:_) -> x
         [] -> 0
     where
+        -- calculate edges only once. this includes converting to fractional type
         edges = verticesToEdges vertices
         both = (vertices, edges)
         rectanglesInsidePolygon = do
@@ -330,117 +344,3 @@ part2 vertices = case areas of
             guard $ x1 /= x2 && y1 /= y2 && polygonInsidePolygonTouchingOk (makeRectangle v w) both
             return (v,w)
         areas = sortOn Down $ fmap (uncurry area) rectanglesInsidePolygon
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- Tests
-
-genTestString = do
-    s <- readFile "test_input"
-    putStrLn $  "tString = " <> show s
-
-tString = "7,1\n11,1\n11,7\n9,7\n9,5\n2,5\n2,3\n7,3\n"
-
-tParsed = case parse parser "input" tString of
-    Right x -> x
-    Left _ -> error "not parsed"
-
--- >>> OnePerLine tParsed
--- (7,1)
--- (11,1)
--- (11,7)
--- (9,7)
--- (9,5)
--- (2,5)
--- (2,3)
--- (7,3)
-
--- >>> OnePerLine $ choose 2 tParsed
--- [(7,1),(11,1)]
--- [(7,1),(11,7)]
--- [(7,1),(9,7)]
--- [(7,1),(9,5)]
--- [(7,1),(2,5)]
--- [(7,1),(2,3)]
--- [(7,1),(7,3)]
--- [(11,1),(11,7)]
--- [(11,1),(9,7)]
--- [(11,1),(9,5)]
--- [(11,1),(2,5)]
--- [(11,1),(2,3)]
--- [(11,1),(7,3)]
--- [(11,7),(9,7)]
--- [(11,7),(9,5)]
--- [(11,7),(2,5)]
--- [(11,7),(2,3)]
--- [(11,7),(7,3)]
--- [(9,7),(9,5)]
--- [(9,7),(2,5)]
--- [(9,7),(2,3)]
--- [(9,7),(7,3)]
--- [(9,5),(2,5)]
--- [(9,5),(2,3)]
--- [(9,5),(7,3)]
--- [(2,5),(2,3)]
--- [(2,5),(7,3)]
--- [(2,3),(7,3)]
-
-t1 = sortOn (\(a,b,c) -> Down a) $ fmap (\(a,b) -> (uncurry area (a,b), a, b)) ((\[a,b] -> (a,b)) <$> choose 2 tParsed)
-
--- >>> OnePerLine t1
--- (15,(11,1),(2,5))
--- (15,(11,7),(2,3))
--- (13,(11,1),(2,3))
--- (13,(11,7),(2,5))
--- (13,(9,7),(2,3))
--- (12,(7,1),(11,7))
--- (11,(7,1),(2,5))
--- (11,(9,7),(2,5))
--- (11,(9,5),(2,3))
--- (10,(7,1),(9,7))
--- (10,(11,1),(9,7))
--- (10,(11,7),(7,3))
--- (9,(7,1),(2,3))
--- (9,(9,5),(2,5))
--- (9,(2,5),(7,3))
--- (8,(7,1),(9,5))
--- (8,(11,1),(11,7))
--- (8,(11,1),(9,5))
--- (8,(11,1),(7,3))
--- (8,(9,7),(7,3))
--- (7,(2,3),(7,3))
--- (6,(7,1),(11,1))
--- (6,(11,7),(9,5))
--- (6,(9,5),(7,3))
--- (4,(7,1),(7,3))
--- (4,(11,7),(9,7))
--- (4,(9,7),(9,5))
--- (4,(2,5),(2,3))
